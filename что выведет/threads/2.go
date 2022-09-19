c9d645e04bc1f26b8e1b2df2422710a69f0a5724
nechel11 <zaf-khasanov@yandex.ru>
2022-09-19 10:59:33 +0300

    asd

--- что выведет/threads/2.go
+++ что выведет/threads/2.go
@@ -12,13 +12,13 @@ func main() {
 	defer cancel()
 
 	select {
-	case <-time.After(1 * time.Second):
-		fmt.Println("waited for 1 sec")
-	case <-time.After(2 * time.Second):
-		fmt.Println("waited for 2 sec")
-	case <-time.After(3 * time.Second):
-		fmt.Println("waited for 3 sec")
-	case <-ctx.Done():
-		fmt.Println(ctx.Err())
+		case <-time.After(1 * time.Second):
+			fmt.Println("waited for 1 sec")
+		case <-time.After(2 * time.Second):
+			fmt.Println("waited for 2 sec")
+		case <-time.After(3 * time.Second):
+			fmt.Println("waited for 3 sec")
+		case <-ctx.Done():
+			fmt.Println(ctx.Err())
 	}
 }
